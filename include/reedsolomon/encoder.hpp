9874e2ee000b61ff53ee58efb674cf65e72d1833
JustRictor <justrictor39@outlook.com>
2024-03-09 20:09:06 +0300

    Добавил файлы кодировщика

---
+++ include/reedsolomon/encoder.hpp
@@ -0,0 +1,18 @@
+#ifndef __ENCODER_HPP_
+#define __ENCODER_HPP_
+
+namespace Rs
+{
+
+
+
+class Encoder
+{
+
+};//Encoder
+
+
+
+}//Rs
+
+#endif//__ENCODER_HPP_
---
+++ src/encoder.cpp
@@ -0,0 +1,1 @@
+#include "reedsolomon/encoder.hpp"
